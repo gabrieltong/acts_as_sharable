f9611127523956a63136598f502c448ba3168536
gabrieltong <tongyangmath@gmail.com>
2013-09-17 08:56:50 +0800

    test

--- lib/acts_as_sharable.rb
+++ lib/acts_as_sharable.rb
@@ -1,5 +1,30 @@
 require 'acts_as_sharable/core_ext'
 
 module ActsAsSharable
+  extend ActiveSupport::Concern
 
+  module ClassMethods
+    def acts_as_commentable
+      before_save do |record|
+        pp "#{record.id}.#{record.title}"
+      end
+      include ActsAsSharable::LocalInstanceMethods
+      extend ActsAsSharable::SingletonMethods
+    end
+  end
+
+  # This module contains class methods
+  module SingletonMethods
+    def size2
+      self.count
+    end
+  end
+
+  module LocalInstanceMethods
+    def name2
+      "#{title} #{title}"
+    end
+  end
 end
+
+ActiveRecord::Base.send(:include, ActsAsSharable)
